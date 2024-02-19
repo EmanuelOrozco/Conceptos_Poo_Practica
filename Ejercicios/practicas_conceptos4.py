fc206d75261aad15ee0e904ee8fa41f59bfb8df6
Emanuel Orozco <emanuelorozco2011@hotmail.com>
2024-02-19 15:29:14 -0500

    Add practica 4 and progress, add practicas5 file

---
+++ Ejercicios/practicas_conceptos4.py
@@ -0,0 +1,12 @@
+# Las clases pueden tener campos de instancia, mas comunmente llamados atributos
+# En Python se pueden asignar atributos arbitrarios
+
+class Circulo:
+    pass
+
+
+c1 = Circulo()
+c2 = Circulo()
+
+c1.radio = 5
+c2.radio = 6
